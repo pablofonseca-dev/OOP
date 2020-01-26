58839a1a089ae88758ffa042dd6eaa868cab4108
Pablo Fonseca <pablo.fonsecam@outlook.com>
2020-01-25 23:14:16 -0600

    Project Updated: Kody Simpson Project #1

--- JavaFX/Kody_Simpson_Tutorial/Video-2/src/ApplicationStart.java
+++ JavaFX/Kody_Simpson_Tutorial/Video-2/src/ApplicationStart.java
@@ -2,7 +2,9 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class ApplicationStart extends Application {
 
@@ -50,6 +52,51 @@ public class ApplicationStart extends Application {
         primaryStage.setResizable(true);
         primaryStage.show();
         //End
+
+        Stage secondStage = new Stage();
+        secondStage.setTitle("Second Window");
+
+        secondStage.setWidth(400);
+        secondStage.setHeight(300);
+
+        secondStage.setX(200);
+        secondStage.setY(500);
+
+        //Modality
+        /*
+        * Represent how you want the stages to interact with each other
+        *
+        * Modality.APPLICATION_MODAL what this will do is that other windows
+        * cannot be used until this one is closed.
+        *
+        * Modality.NONE is the default value where you can interact with any window
+        *
+        * Modality.WINDOW_MODAL means that you cannot mess with the window
+        * that owns this window until this windows closed. We need to give an owner
+        * for the child window.
+        * */
+        Stage newWindow = new Stage();
+        newWindow.initModality(Modality.WINDOW_MODAL);
+        newWindow.initOwner(secondStage);
+        //Init Style
+        /*
+        * Useful to initialize styles
+        * StageStyle.DECORATED is the regular window, so we can change how the window
+        * appears.
+        * StageStyle.TRANSPARENT basically gonna make it invisible.
+        * StageStyle.UTILITY useful in case you ever want to have like a simple
+        * tool pop-up or like a Settings menu. That's basically what I would use it.
+        * StageStyle.UNDECORATED is invisible because it's only going to show
+        * constants of the window it's only going to show the constants of the window
+        * it's not going to show the frame of the window. An undecorated window it's going
+        * to be a regular window but with only the contents not the frame.
+        * StageStyle.UNIFIED removes the border between the decoration area and the main content
+        * area
+        * */
+        newWindow.initStyle(StageStyle.UNIFIED);
+        secondStage.show();
+
+        newWindow.show();
     }
 
     @Override
